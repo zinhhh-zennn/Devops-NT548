28c98cb27f97ea19b572041934b970c68ce8ec84
zinhhh-zennn <vvvinhng2004@gmail.com>
2026-01-18 20:11:58 +0700

    sua code vairiable.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,7 @@ variable "region" { default = "ap-southeast-2" }
 variable "my_ip_address" {
   description = "IP Public của bạn để SSH (ví dụ: 14.161.x.x/32)"
   type        = string
+  default     = "0.0.0.0/0"
 }
 variable "key_pair_name" {
   description = "Tên KeyPair đã tạo trên AWS"
